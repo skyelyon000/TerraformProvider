e1a9e1309324433437d64e11ad2b75c8d2673ae6
Skye <skyel@spacelift.io>
2025-03-19 10:42:43 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,7 @@ resource "spacelift_stack" "ec2-stack" {
   name         = "development-stack"
   repository   = "terraform-starter"
 }
+module "my-first-mod" {
+  source  = "spacelift.io/skyelyon000-github/my-first-mod/default"
+  version = "0.1.0"
+}
